4474d893b56d5f3594a7e9e0caadbe0f703e6386
hussain.tamboli <hussain.tamboli@lazada.com>
2018-04-12 17:40:25 +0800

    basic app with Home and Header component

---
+++ src/app/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Header Component</h3>
+            </div>
+        )
+    }
+}
---
+++ src/app/index.js
@@ -0,0 +1,23 @@
+console.log("it works!")
+import React from "react"
+import { render } from "react-dom"
+
+import { Header } from "./components/Header";
+import { Home } from "./components/Home";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <Header/>
+                </div>
+                <div>
+                    <Home/>
+                </div>
+            </div>
+        )
+    }
+}
+
+render(<App/>, window.document.getElementById("app"))
