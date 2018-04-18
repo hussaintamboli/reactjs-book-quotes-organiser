dbcbbef5bfd67d90b9a353bb427e6414c6ff52d7
Hussain <hussaintamboli18@gmail.com>
2018-04-18 15:37:47 +0800

    Added a text area for adding quote. And removed package-lock.json from .gitignore

--- src/app/components/Quotes.js
+++ src/app/components/Quotes.js
@@ -4,6 +4,10 @@ const queryString = require('query-string');
 
 export class Quotes extends React.Component {
 
+    addQuote() {
+        console.log("adding quote..")
+    }
+
     render() {
         const parsedQs = queryString.parse(location.search);
         var title = undefined;
@@ -24,8 +28,17 @@ export class Quotes extends React.Component {
         }
         return (
             <div className="col-md-12">
+                <div className="row">
+                    <div className="form-group col-md-12">
+                        <textarea 
+                                className="form-control col-md-12" 
+                                rows="2" placeholder="An awesome quote"/>
+                        </div>
+                    <div className="form-group col-md-12">
+                        <button onClick={this.addQuote.bind(this)} className="btn btn-primary float-right">Add</button>
+                    </div>
+                </div>
                 <p className="lead">Quotes { title !== undefined ? 'from ' + title : 'by ' + author }</p>
-
                 <ul>
                     {this.state.quotes.map((item, i) => <li key={i}>{item}</li>)}
                 </ul>
