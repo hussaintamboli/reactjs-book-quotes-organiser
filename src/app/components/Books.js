90c4fc7e63ee94ea293c300b8233f1574cdec06b
Hussain <hussaintamboli18@gmail.com>
2018-04-25 12:43:21 +0800

    Fixed child calling a function from parent for updating parent state

--- src/app/components/Books.js
+++ src/app/components/Books.js
@@ -1,70 +1,35 @@
 import React from "react";
 
 import { BookList } from "./BookList";
+import { NewBook } from "./NewBook";
+
 
 export class Books extends React.Component {
-    constructor(props) {
+    constructor() {
         super();
         this.state = {
             books: [
                 {title: "The Black Swan", author: "Nassim Nicholas Taleb"},
                 {title: "The Lean Startup", author: "Eric Ries"}
-            ],
-            new: {
-                title: '',
-                author: ''
-            }
+            ]
         };
     }
 
-    addBook() {
-        console.log("adding..");
+    addBook = (book) => {
+        console.log("adding ", book);
         const newBooks = this.state.books;
-        newBooks.push(this.state.new);
+        newBooks.push(book);
         this.setState({
-            books: newBooks,
-            new: {
-                title: '',
-                author: ''
-            }
+            books: newBooks
         });
-    }
-
-    updateTitle(event) {
-        const newState = Object.assign({}, this.state);
-        newState.new.title = event.target.value;
-        this.setState(newState);
-    }
-
-    updateAuthor(event) {
-        const newState = Object.assign({}, this.state);
-        newState.new.author = event.target.value;
-        this.setState(newState);
     }
 
     render() {
         return (
-            <div className="col-md-6 offset-3">
-                <div className="form-group">
-                    <textarea 
-                        className="form-control col-md-12" 
-                        rows="2" placeholder="I am reading this book named" 
-                        value={this.state.new.title}
-                        onChange={(event) => this.updateTitle(event)} />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="author" className="font-italic font-weight-light">by</label>
-                    <input className="form-control form-control-sm col-md-12" 
-                        id="author" placeholder="this author" 
-                        value={this.state.new.author}
-                        onChange={(event) => this.updateAuthor(event)}/> 
-                </div>
-                <div className="form-group">
-                    <button onClick={this.addBook.bind(this)} className="btn btn-primary btn-lg btn-block">Add</button>
-                </div>
+            <div className="col-md-6 offset-3"> 
+                <NewBook onAdd={this.addBook} defaultTitle={''} defaultAuthor={''} />
 
                 <BookList books={this.state.books}/>
-
             </div>
         );
     }
--- src/app/components/NewBook.js
+++ src/app/components/NewBook.js
@@ -1,9 +1,48 @@
 import React from "react";
 
 export class NewBook extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            title: props.defaultTitle,
+            author: props.defaultAuthor
+        };
+    }
+
+    add() {
+        this.props.onAdd({title: this.state.title, author: this.state.author});
+        this.setState({title: this.props.defaultTitle, author: this.props.defaultAuthor});
+    }
+
+    updateTitle(event) {
+        this.setState({title: event.target.value});
+    }
+
+    updateAuthor(event) {
+        this.setState({author: event.target.value});
+    }
+
     render() {
         return (
-            <div>input for adding books</div>
+            <div>
+                <div className="form-group">
+                    <textarea 
+                        className="form-control col-md-12" 
+                        rows="2" placeholder="I am reading this book named" 
+                        value={this.state.title}
+                        onChange={(event) => this.updateTitle(event)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="author" className="font-italic font-weight-light">by</label>
+                    <input className="form-control form-control-sm col-md-12" 
+                        id="author" placeholder="this author" 
+                        value={this.state.author}
+                        onChange={(event) => this.updateAuthor(event)}/> 
+                </div>
+                <div className="form-group">
+                    <button disabled={this.state.title === this.props.defaultTitle || this.state.author === this.props.defaultAuthor} onClick={() => this.add()} className="btn btn-primary btn-lg btn-block">Add</button>
+                </div>
+            </div>
         );
     }
 }
