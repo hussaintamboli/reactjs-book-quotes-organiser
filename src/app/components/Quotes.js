20a2e05a8b65684fa70cdd85f158ceccf43783b3
Hussain <hussaintamboli18@gmail.com>
2018-04-30 11:55:15 +0800

    also fetching quotes by author

--- src/app/components/Quotes.js
+++ src/app/components/Quotes.js
@@ -9,12 +9,60 @@ export class Quotes extends React.Component {
     constructor() {
         super();
         this.state = {
+            fromOrBy: '',
             quotes: [
                 "Ideas come and go, stories stay",
                 "Prediction, not narration, is the real test of our understanding of the world",
                 "Read books are far less valuable than unread ones"
             ]
         }
+    }
+
+    componentDidMount() {
+        const parsedQs = queryString.parse(location.search);
+        if (parsedQs.title) {
+            this.fetchBookQuotes(parsedQs.title);
+        } else if (parsedQs.author) {
+            this.fetchAuthorQuotes(parsedQs.author);
+        } else {
+            console.log("title/author not found in", parsedQs);
+        }      
+    }
+
+    fetchBookQuotes = (key) => {
+        let self = this;
+        fetch('https://bookquotes-f6c75.firebaseio.com/library/' + key + '.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            self.setState({fromOrBy: 'from ' + json.title + ' by ' + json.author});
+            let quotes = json.quotes || [];
+            self.setState({quotes: quotes});
+        })
+        .catch((error) => {
+            console.log("fetchBookQuotes error ", error);
+        });
+    }
+
+    snakeCase (string) {
+        return string.replace(/\s+/g, '-').toLowerCase();
+    }
+
+    fetchAuthorQuotes = (author) => {
+        let self = this;
+        self.setState({fromOrBy: 'by ' + author});
+        let key = this.snakeCase(author);
+        fetch('https://bookquotes-f6c75.firebaseio.com/authors/' + key + '/quotes.json')
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            self.setState({quotes: Object.values(json)})
+        })
+        .catch((error) => {
+            console.log("fetchBookQuotes error ", error);
+        });
     }
 
     addQuote = (quote) => {
@@ -25,25 +73,11 @@ export class Quotes extends React.Component {
     }
 
     render() {
-        const parsedQs = queryString.parse(location.search);
-        var title = undefined;
-        var author = undefined;
-        var fromOrBy = '';
-        if (parsedQs.title) {
-            title = parsedQs.title;
-            fromOrBy = 'from ' + title;
-        } else if (parsedQs.author) {
-            author = parsedQs.author;
-            fromOrBy = 'by ' + author;
-        } else {
-            console.log("title/author not found in", parsedQs);
-        }
-
         return (
             <div className="col-md-12">
-                <NewQuote fromOrBy={fromOrBy} onAdd={this.addQuote} />
+                <NewQuote fromOrBy={this.state.fromOrBy} onAdd={this.addQuote} />
                 
-                <QuoteList fromOrBy={fromOrBy} quotes={this.state.quotes} />
+                <QuoteList fromOrBy={this.state.fromOrBy} quotes={this.state.quotes} />
             </div>
         );
     }
